67db3945ed39ce0cfe67549c4dcc3dc3dfc7dcbb
oryna <ec20251@qmul.ac.uk>
2022-03-24 12:02:29 +0000

    more info btn works

--- components/expense-menu.tsx
+++ components/expense-menu.tsx
@@ -1,5 +1,17 @@
 
 const ExpenseMenu = () => {
+
+    function RevealMoreInfo(): void { 
+        var x = document.getElementById("moreInfo"); 
+        if (x != null) {
+            if ( x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
+    } 
+
     return (
         <>
             <div className="hero min-h-screen bg-base-200 mb-10">
@@ -22,7 +34,7 @@ const ExpenseMenu = () => {
                                                 <span className="w-32 bg-[#0369a1]">Amount</span>
                                                 <input type="text" placeholder="10" className="input input-bordered w-28 bg-neutral"></input>
                                                 <select className="bg-[#0369a1] w-20 text-center no-arrow focus:outline-none">
-                                                    <option selected className=" m">GBP</option>
+                                                    <option selected className="">GBP</option>
                                                     <option>USD</option>
                                                     <option>EUR</option>
                                                     <option>JPY</option>
@@ -41,6 +53,7 @@ const ExpenseMenu = () => {
                                                     <option disabled selected>Pick category</option>
                                                     <option>Travel</option>
                                                     <option>Hospitality</option>
+                                                    <option>Other</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -96,7 +109,7 @@ const ExpenseMenu = () => {
                                                 <span className="w-32 bg-secondary">Amount</span>
                                                 <input type="text" placeholder="10" className="input bg-neutral input-bordered w-28"></input>
                                                 <select className="bg-secondary w-20 text-center no-arrow focus:outline-none">
-                                                    <option selected className=" m">GBP</option>
+                                                    <option selected className="">GBP</option>
                                                     <option>USD</option>
                                                     <option>EUR</option>
                                                     <option>JPY</option>
@@ -115,6 +128,7 @@ const ExpenseMenu = () => {
                                                     <option disabled selected>Pick category</option>
                                                     <option>Travel</option>
                                                     <option>Hospitality</option>
+                                                    <option>Other</option>
                                                 </select>
                                             </div>
                                         </div>
@@ -147,19 +161,56 @@ const ExpenseMenu = () => {
                                             <button className="btn bg-secondary border-none outline text-center w-36 hover:opacity-40 ">Attach File</button>
                                         </div>
 
-                                        <p className="text-sm underline cursor-pointer mt-2 hover:opacity-90">Don't have a reciept? Click here.</p>
+                                        <p className="text-sm underline cursor-pointer mt-2 hover:opacity-90" onClick={RevealMoreInfo}>Don't have a reciept? Click here.</p>
 
                                     </form>
 
                                     <div className="card-actions justify-center">
                                         <button type="submit" className=" btn w-80 bg-indigo-600 mt-1  border-transparent border-2  rounded-2xl text-white font-semibold  hover:opacity-40 mb-[-0.3rem]">Submit</button>
                                     </div>
+
                                 </div>
+
+                            </div>
+                            {/* First div is needed for flex to work */}
+                            <div className="w-full h-1 "></div>
+                            <div id="moreInfo" className="w-full h-96 bg-accent mt-[-8rem] z-50 flex flex-col items-center px-8 pt-3 hidden">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">More Information</span>
+                                    </label>
+
+                                    <label className="input-group py-1">
+                                        <span className="w-32 bg-secondary">Travel Company</span>
+                                        <input type="text" placeholder="WizzAir" className="input  input-bordered w-50 bg-neutral"></input>
+                                    </label>
+
+                                    <label className="input-group py-1">
+                                        <span className="w-32 bg-secondary ">Ticket Number</span>
+                                        <input type="text" placeholder="A6" className="input input-bordered w-50 bg-neutral"></input>
+                                    </label>
+
+                                    <label className="input-group py-1">
+                                        <span className="w-32 bg-secondary">Time, Date of Departure</span>
+                                        <input type="text" placeholder="hh:mm dd/mm/yy" className="input input-bordered w-50 bg-neutral"></input>
+                                    </label>
+
+                                    <label className="input-group py-1">
+                                        <span className="w-32 bg-secondary ">Reference Number</span>
+                                        <input type="text" placeholder="XXXXXXXX" className="input input-bordered w-50 bg-neutral"></input>
+                                    </label>
+                                    
+                                </div>
+
+                                <div className="card-actions justify-center">
+                                        <button type="submit" className=" btn w-80 bg-indigo-600 mt-8  border-transparent border-2  rounded-2xl text-white font-semibold  hover:opacity-40 mb-[-0.3rem]">Submit</button>
+                                </div>
+
                             </div>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         </>
 
