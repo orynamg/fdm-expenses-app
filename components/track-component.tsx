6ce807150d4f858a75e58b5616c9c8deb277c3dc
oryna <ec20251@qmul.ac.uk>
2022-03-25 13:47:36 +0000

    menu collapse works

--- components/track-component.tsx
+++ components/track-component.tsx
@@ -1,33 +1,50 @@
 const TrackComponent = () => {
 
+
     const items = [
         {
             "date": "Jul 5, 2021",
-            "expense": 245.00,
+            "amount": 245.00,
             "ccy": "£",
             "type": "Hospitality",
-            "card": "Visa****"
+            "card": "Visa****",
+            "expense": "Small",
+            "appeal": "None",
+            "statement": "None",
+            "lineManager": "John Hudson"
         },
         {
             "date": "Apr 29, 2021",
-            "expense": 34.00,
+            "amount": 34.00,
             "ccy": "£",
             "type": "Travel",
-            "card": "Visa****"
+            "card": "Visa****",
+            "expense": "Small",
+            "appeal": "None",
+            "statement": "None",
+            "lineManager": "John Hudson"
         },
         {
             "date": "Apr 21, 2021",
-            "expense": 575.00,
+            "amount": 575.00,
             "ccy": "£",
             "type": "Travel",
-            "card": "MasterCard****"
+            "card": "MasterCard****",
+            "expense": "Large",
+            "appeal": "None",
+            "statement": "None",
+            "lineManager": "John Hudson"
         },
         {
             "date": "Mar 26, 2021",
-            "expense": 54.57,
+            "amount": 54.57,
             "ccy": "£",
             "type": "Travel",
-            "card": "Visa****"
+            "card": "Visa****",
+            "expense": "Small",
+            "appeal": "None",
+            "statement": "None",
+            "lineManager": "John Hudson"
         }
     ]
 
@@ -47,33 +64,44 @@ const TrackComponent = () => {
                         </div>
 
                         {
-                            items.map((item, index) => {
-                                if (index % 2 == 0)
-                                    return (
-                                        <div className="w-full h-24 lighter mx-0 px-0 grid grid-cols-5 items-center justify-center text-center text-slate-300">
+                            items.map((item, index) => 
+                                (
+                                    <div key={index} tabIndex={0} className="collapse collapse-arrow">
+                                       
+                                        
+                                        <div className={"w-full h-24 mx-0 px-0 grid grid-cols-4 items-center justify-center text-center text-slate-300 collapse-title " +( (index%2===0) ? "lighter" : "darker") }>
                                             <p className="text-slate-400">{item.date}</p>
-                                            <p className="text-left">{item.ccy}{item.expense.toFixed(2)}</p>
+                                            <p className="text-left">{item.ccy}{item.amount.toFixed(2)}</p>
                                             <p className="text-slate-400 text-right">{item.type}</p>
-                                            <p className="ml-20">{item.card}</p>
-                                            <p className="underline text-sm cursor-pointer hover:opacity-90">View More</p>
+                                            <p className="ml-30">{item.card}</p>
                                         </div>
-                                    )
-                                else 
-                                    return (
-                                        <div className="w-full h-24 darker mx-0 px-0 grid grid-cols-5 items-center justify-center text-center text-slate-300">
-                                            <p className="text-slate-400">{item.date}</p>
-                                            <p className="text-left">{item.ccy}{item.expense.toFixed(2)}</p>
-                                            <p className="text-slate-400 text-right">{item.type}</p>
-                                            <p className="ml-20">{item.card}</p>
-                                            <p className="underline text-sm cursor-pointer hover:opacity-90">View More</p>
+                                        {/* <input type="checkbox" className="peer"></input>  */}
+
+                                        <div className={"collapse-content w-full h-24 mx-0 px-0 grid grid-cols-5 items-center justify-center text-center text-slate-300 " + ( (index%2===0) ? "lighter" : "darker") } >
+                                            <p className="text-slate-400">{item.expense} Expense</p>
+                                            <p className="text-left">Appealed: {item.appeal}</p>
+                                            <p className="text-slate-400">Appeal Statement: {item.statement}</p>
+                                            <p className="">Line Manager:  {item.lineManager}</p>
+                                            <p className="underline text-sm cursor-pointer hover:opacity-90">View Attachment</p>
                                         </div>
-                                    )
-                            })
+
+                                    </div>
+                                )
+                            )
                         }
 
-                        <div className="w-full h-10 dark-primary mx-0 px-0 grid grid-cols-1 items-center justify-center text-center text-slate-300 text-sm opacity-90">
+                        <div className="w-full h-10 dark-primary mx-0 px-0 grid grid-cols-5 items-center justify-center text-center text-slate-300 text-sm opacity-90">
+                            {/* Need empty <p></p> for grid to form correctly */}
+                            <p></p> 
+                            <p></p>
                             <p className="underline hover:opacity-90 cursor-pointer">View More Entries</p>
-
+                            {/* Need empty <p></p> for grid to form correctly */}
+                            <p></p>
+                            <div className="btn-group">
+                                <button className="btn">«</button>
+                                <button className="btn">Page 1</button>
+                                <button className="btn">»</button>
+                            </div>
                         </div>
 
                     </div>
